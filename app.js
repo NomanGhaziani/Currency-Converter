410a914d3e4fb21eed6e963c8a04643c6f6b9d47
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-05-04 03:17:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+import inquirer from 'inquirer';
+let currencyconverter = {
+    "USD": {
+        "USD": 1,
+        "EUR": 0.92,
+        "PKR": 278
+    },
+    "EUR": {
+        "USD": 1.08,
+        "EUR": 1,
+        "PKR": 300,
+    },
+    "PKR": {
+        "USD": 0.0036,
+        "EUR": 0.0033,
+        "PKR": 1,
+    }
+};
+//imput prompt
+const answer = await inquirer.prompt([{
+        name: "from",
+        message: "Enter your currency which you want to convert?",
+        type: "list",
+        choices: ["USD", "EUR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "Select which currency convert?",
+        type: "list",
+        choices: ["USD", "EUR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "Enter your amount in number",
+        type: "number",
+        choices: ["USD", "EUR", "PKR"],
+    }]);
+const { from, to, amount } = answer;
+//conversion
+if (from && to && amount) {
+    let result = currencyconverter[from][to] * amount;
+    console.log(`your conversion from ${from} to ${to} is ${result}`);
+}
+else {
+    console.log("invalid operater select");
+}
